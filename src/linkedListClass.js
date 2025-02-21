20545c77f177a02f7542c6e273fc5db04bdd60bd
snooze26 <langdon.douglas26@gmail.com>
2025-02-21 10:21:34 -0600

    remove factory function

--- src/linkedListClass.js
+++ src/linkedListClass.js
@@ -51,37 +51,3 @@ console.log("After first prepend", testList);
 
 // console.log(testList);
 console.log(testList.getSize());
-// export function linkedList() {
-//     let list = []; 
-//     let head = new node();
-
-//     const appendToEnd = function(head) {
-//         let temp = head; 
-
-//         if (temp === null) {
-//             prependToFront(temp);
-//         } 
-//         while (temp.next != null) {
-//             temp = temp.next;
-//             temp.next = new node(item, null);
-//         }
-//         return {head, temp};
-//     }
-
-//     const prependToFront = function(head) {
-//         console.log("new node" , head);
-//         if (head == null) console.error("Here is error my boy");
-//         return 
-    
-//     }
-
-//     return {appendToEnd, head, prependToFront};
-
-// }
-
-// const newNode = new node("Test", "next");
-// // console.log(newNode); 
-
-// const list = new linkedList();
-// list.prependToFront(newNode);
-// console.log(list.toString());
