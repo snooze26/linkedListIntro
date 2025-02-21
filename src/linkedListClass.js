4641c836ba5347e86a7a968925d298739d9bcb1a
snooze26 <langdon.douglas26@gmail.com>
2025-02-21 11:17:10 -0600

    polish up append/prepend to work on at()

--- src/linkedListClass.js
+++ src/linkedListClass.js
@@ -9,26 +9,28 @@ import { node } from "./nodeClass.js"
             this.size = 0;
         }
 
-        
-        append(node) {
-            if (!this.head) {
+        prepend(node) {
+            if(!this.tail) { // no tail assign tail to current node 
                 this.head = node;
-                this.tail = node; 
-            } else { 
-                this.tail = node; 
+                this.tail = node 
+            } else {
+            node.next = this.head;  // assign node to head 
+            this.head = node; 
             }
             this.size++
 
         }
-
-        prepend(node) {
-            if(!this.tail) { 
-                this.head = node;
-                this.tail = node 
-                
-            }
-            this.head = node;
+        
+        append(node) {
+            if (this.head == null) return this.prepend(node);  // if no head then add new node to the beginning 
+            let current = this.head; 
+            while(current.next != null) { // while the current node next field is not empty 
+                current = current.next; // assign this.head to the next node 
+            };
+            current.next = node; 
+            this.tail = node; 
             this.size++;
+
         }
         
         getSize() { 
@@ -59,15 +61,20 @@ import { node } from "./nodeClass.js"
 
 let testList = new linkedList(); 
 const testNode3 = new node(7, null);
-const testNode = new node(5, testNode3);
-const testNode2 = new node(6, testNode);
+const testNode = new node(5, null);
+const testNode2 = new node(6, null);
 
 
 
 
 
 testList.prepend(testNode);
+console.log(testList, "first run APPEND")
 testList.append(testNode3);
-testList.append(testNode2)
+console.log("second run APPEND", testList)
+testList.prepend(testNode2)
+console.log("third run PREPEND", testList)
 
-console.log(testList.at(1));
+
+
+
